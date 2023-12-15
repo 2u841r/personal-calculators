929ed4fac0fa05d804ed3d3c022f3dd2613f2942
Zubair <77893877+2u841r@users.noreply.github.com>
2023-12-15 15:52:12 +0600

    currency converter added

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-function foodPrice(){
+function foodPrice() {
     let weight = document.querySelector('#weight').value
     let price = document.querySelector('#price').value
-    console.log('per kg ' + parseInt((price / weight * 1000)));
+
     let perKG = parseInt((price / weight * 1000))
-    document.querySelector('#foodPriceResult').innerHTML = `Per kg: ${perKG} ` 
-    
+    document.querySelector('#foodPriceResult').innerHTML = `Per kg: ${perKG} `
+
 }
 
 
-function TilesToSize(){
+function TilesToSize() {
     let tilesSize = document.querySelector('#tilesSize').value
     let xRow = document.querySelector('#xRow').value
     let yColumn = document.querySelector('#yColumn').value
-    console.log('per kg ' + parseInt((price / weight * 1000)));
 
-    let squareFeet = parseInt((tilesSize * tilesSize / 12 / 12 * xRow * yColumn ))
-    document.querySelector('#homeSize').innerHTML = `Home size: ${squareFeet} square feet` 
-    
+
+    let squareFeet = parseInt((tilesSize * tilesSize / 12 / 12 * xRow * yColumn))
+    document.querySelector('#homeSize').innerHTML = `Home size: ${squareFeet} square feet`
+
+}
+
+let exchangeRate;
+let foreignCurrency;
+function F2L() {
+    exchangeRate = document.querySelector('#exchangeRate').value;
+
+    foreignCurrency = document.querySelector('#foreignCurrency').value;
+
+    let soYouNeed = (1 / exchangeRate * foreignCurrency);
+
+    document.querySelector('#localCurrency').value = `${soYouNeed}`
+
+
+}
+
+let localCurrency;
+function L2F() {
+    localCurrency = document.querySelector('#localCurrency').value;
+
+    let soYouNeed = (localCurrency * exchangeRate);
+
+    document.querySelector('#foreignCurrency').value = `${soYouNeed}`
+
 }
 
